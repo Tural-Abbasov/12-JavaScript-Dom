d1bcda3a4af81415df48754a24e1b33ed96e49ab
Tural <166192406+Tural-Abbasov@users.noreply.github.com>
2024-08-11 15:42:38 +0400

    Add files via upload

---
+++ assets/js/quiz.js
@@ -0,0 +1,563 @@
+var questionBank = [
+
+
+  // Az/dili Asan level questions
+
+
+  {
+    question: "1. Nece? Ne qeder? Necenci? suallari hansi nitq hissesinin suallaridir?",
+    answers: {
+      A: "A) Sifetin",
+      B: "B) Sayin",
+      C: "C) İsmin",
+      D: "D) Zerfin",
+    },
+    correct: function () {
+      return this.answers.B;
+    },
+    subject: "Az/dili",
+    level: "Asan",
+  },
+  {
+    question: "2. Verilenlerden biri esas nitq hisselerine aid deyil:",
+    answers: {
+      A: "A) Sifet",
+      B: "B) Fel",
+      C: "C) Edat",
+      D: "D) İsim",
+    },
+    correct: function () {
+      return this.answers.C;
+    },
+    subject: "Az/dili",
+    level: "Asan",
+  },
+  {
+    question: "3. Nitq hisseleri diliciliyin hansi shobesinde oyrenilir?",
+    answers: {
+      A: "A) Morfologiya",
+      B: "B) Leksikologiya",
+      C: "C) Fonetika",
+      D: "D) Sintaksis",
+    },
+    correct: function () {
+      return this.answers.A;
+    },
+    subject: "Az/dili",
+    level: "Asan",
+  },
+  {
+    question: "4. Hansi nitq hissesi deyil?",
+    answers: {
+      A: "A) Sifet",
+      B: "B) Leksika",
+      C: "C) Edat",
+      D: "D) Qoshma",
+    },
+    correct: function () {
+      return this.answers.B;
+    },
+    subject: "Az/dili",
+    level: "Asan",
+  },
+  {
+    question: "5. Hanse isimdir?",
+    answers: {
+      A: "A) chirkli",
+      B: "B) shirin",
+      C: "C) qacmaq",
+      D: "D) Perviz",
+    },
+    correct: function () {
+      return this.answers.D;
+    },
+    subject: "Az/dili",
+    level: "Asan",
+  },
+
+
+  // Az/dili Orta level questions
+
+
+  {
+    question: "1. Ashagidakilardan biri soz birleşmesi deyil:",
+    answers: {
+      A: "A) Veten ucun",
+      B: "B) Boyuk sheher",
+      C: "C) Kitabi oxumaq",
+      D: "D) Dag havasi",
+    },
+    correct: function () {
+      return this.answers.A;
+    },
+    subject: "Az/dili",
+    level: "Orta",
+  },
+  {
+    question: "2. Soz birleshmeleri qurulushuna gerə necə cur olur?",
+    answers: {
+      A: "A) 3",
+      B: "B) 2",
+      C: "C) 4",
+      D: "D) 1",
+    },
+    correct: function () {
+      return this.answers.B;
+    },
+    subject: "Az/dili",
+    level: "Orta",
+  },
+  {
+    question: "3. Cumlede mueyyen sozun digerlerine nisbeten quvvetli deyilmesine ..... deyilir. Noqtelerin yerine ne olmalidir?",
+    answers: {
+      A: "A) mentiqi vurğu",
+      B: "B) vurgu",
+      C: "C) aheng qanunu",
+      D: "D) soz sirasi",
+    },
+    correct: function () {
+      return this.answers.A;
+    },
+    subject: "Az/dili",
+    level: "Orta",
+  },
+  {
+    question: "4. Birinci nov teyini soz birleshmesinin asili terefi hansi cumle uzvu olur?",
+    answers: {
+      A: "A) zerflik",
+      B: "B) tamamliq",
+      C: "C) mubteda",
+      D: "D) teyin",
+    },
+    correct: function () {
+      return this.answers.D;
+    },
+    subject: "Az/dili",
+    level: "Orta",
+  },
+  {
+    question: "5. 'Veten' sozunun leksik tehlilindeki sozu gosterin:",
+    answers: {
+      A: "A) Sinonimi yoxdur",
+      B: "B) Qurbet sozunun antonimidir",
+      C: "C) Umumishlek sozdur",
+      D: "D) Omonimliyi yoxdur",
+    },
+    correct: function () {
+      return this.answers.A;
+    },
+    subject: "Az/dili",
+    level: "Orta",
+  },
+
+
+  // Az/dili Cetin level questions
+
+
+  {
+    question: "1. Biri Azerbaycan edebi dilinin normasi deyil:",
+    answers: {
+      A: "A) Leksik",
+      B: "B) Piktoqrafiq",
+      C: "C) Qrammatik",
+      D: "D) Fonetik",
+    },
+    correct: function () {
+      return this.answers.B;
+    },
+    subject: "Az/dili",
+    level: "Cetin",
+  },
+  {
+    question: "2. Biri edebi dilin shifahi formasina numunə kimi verile biler:",
+    answers: {
+      A: "A) Elmi kitablar",
+      B: "B) Muhazireler",
+      C: "C) Reklamlar",
+      D: "D) Bedii eserler",
+    },
+    correct: function () {
+      return this.answers.B;
+    },
+    subject: "Az/dili",
+    level: "Cetin",
+  },
+  {
+    question: "3. Hansi numunede edebi dilin orfoepik normasi pozulmushdur:",
+    answers: {
+      A: "A) [ me:llim ]",
+      B: "B) [ otax ]",
+      C: "C) [ shefek ]",
+      D: "D) [ mennen ]",
+    },
+    correct: function () {
+      return this.answers.C;
+    },
+    subject: "Az/dili",
+    level: "Cetin",
+  },
+  {
+    question: "4. Edebi dilin hansi normasi sozler ve cumleler arasindaki elaqenin nitqde duzgun qurulmasini teleb edir?",
+    answers: {
+      A: "A) Qrammatik",
+      B: "B) Fonetik",
+      C: "C) Həm qrammatik, həm fonetik",
+      D: "D) Leksik",
+    },
+    correct: function () {
+      return this.answers.A;
+    },
+    subject: "Az/dili",
+    level: "Cetin",
+  },
+  {
+    question: "5. Cemiyyetin butun sahelerini ehate etmesi ile ashagidakilardan biri digerlerinden ferqlenir:",
+    answers: {
+      A: "A) Dil",
+      B: "B) Siyaset",
+      C: "C) İqtisadiyyat",
+      D: "D) Medeniyyet",
+    },
+    correct: function () {
+      return this.answers.A;
+    },
+    subject: "Az/dili",
+    level: "Cetin",
+  },
+
+
+  // Informatika Asan level questions
+
+
+  {
+    question: "1. Proqram Nedir?",
+    answers: {
+      A: "A) Kodlashdirmadir",
+      B: "B) Dovru alqoritmidir",
+      C: "C) Alqoritmidir",
+      D: "D) Alqoritmin mashin dilində yazilishidir",
+    },
+    correct: function () {
+      return this.answers.B;
+    },
+    subject: "İnformatika",
+    level: "Asan",
+  },
+  {
+    question: "2. Pascal programlashdirma dilinin yaradicisi kim olub?",
+    answers: {
+      A: "A) Nicklaus Wirth",
+      B: "B) Steve Jobs",
+      C: "C) Bill Gates",
+      D: "D) Blaise Pascal",
+    },
+    correct: function () {
+      return this.answers.A;
+    },
+    subject: "İnformatika",
+    level: "Asan",
+  },
+  {
+    question: "3. Ashagidakilardan hansi proqramlashdirma dilidir?",
+    answers: {
+      A: "A) MS Word",
+      B: "B) Turbo Packal",
+      C: "C) Photoshop",
+      D: "D) Adobe Reader",
+    },
+    correct: function () {
+      return this.answers.B;
+    },
+    subject: "İnformatika",
+    level: "Asan",
+  },
+  {
+    question: "4. En cox istifade edilen vereqler hansilardir?",
+    answers: {
+      A: "A) A3",
+      B: "B) A4",
+      C: "C) A8",
+      D: "D) A5",
+    },
+    correct: function () {
+      return this.answers.B;
+    },
+    subject: "İnformatika",
+    level: "Asan",
+  },
+  {
+    question: "5. İlk obyekt-yonlu proqramlashdirma dili hansidir?",
+    answers: {
+      A: "A) Delphi",
+      B: "B) Pascal",
+      C: "C) C++",
+      D: "D) Lazarus",
+    },
+    correct: function () {
+      return this.answers.C;
+    },
+    subject: "İnformatika",
+    level: "Asan",
+  },
+
+
+  // Informatika Orta level questions
+
+
+  {
+    question: "1. Dunyada ilk veb sayt necenci ilde yaradilmishdir?",
+    answers: {
+      A: "A) 1991-ci ilde avqustun 6-da",
+      B: "B) 1999-cu ilde sentyabrin 6-da",
+      C: "C) 1980-1985 ci illerde",
+      D: "D) 1950-ci illerde",
+    },
+    correct: function () {
+      return this.answers.A;
+    },
+    subject: "İnformatika",
+    level: "Orta",
+  },
+  {
+    question: "2. WWW menasi nedir?",
+    answers: {
+      A: "A) Lokal shebeke",
+      B: "B) Elektron poct",
+      C: "C) Dunya Horumcek Toru",
+      D: "D) Qlobal sgebeke",
+    },
+    correct: function () {
+      return this.answers.C;
+    },
+    subject: "İnformatika",
+    level: "Orta",
+  },
+  {
+    question:"3. Ashagidakilardan hansi axtarish sistemi deyil?",
+    answers: {
+      A: "A) Opera",
+      B: "B) Google",
+      C: "C) Yahoo",
+      D: "D) Ask Jeeves",
+    },
+    correct: function () {
+      return this.answers.A;
+    },
+    subject: "İnformatika",
+    level: "Orta",
+  },
+  {
+    question: "4. İnternetin qloballashmasi necenci illeri ehate edir?",
+    answers: {
+      A: "A) 1986-1987",
+      B: "B) 2000-2005",
+      C: "C) 1980-1985",
+      D: "D) 1988-1989",
+    },
+    correct: function () {
+      return this.answers.D;
+    },
+    subject: "İnformatika",
+    level: "Orta",
+  },
+  {
+    question: "5. Hansi modemin surətinin olcu vahididir?",
+    answers: {
+      A: "A) hers",
+      B: "B) Kbit/saat",
+      C: "C) bps bit/san",
+      D: "D) m/san",
+    },
+    correct: function () {
+      return this.answers.C;
+    },
+    subject: "İnformatika",
+    level: "Orta",
+  },
+
+
+  // Informatika Cetin level questions
+
+
+  {
+    question: "1. Domen nedir?",
+    answers: {
+      A: "A) Veb saytlarin uzantisi",
+      B: "B) hec biri",
+      C: "C) Veb saytlarin İP unvani",
+      D: "D) Veb saytlarin adi",
+    },
+    correct: function () {
+      return this.answers.D;
+    },
+    subject: "İnformatika",
+    level: "Cetin",
+  },
+  {
+    question: "2. Google axtarish sistemi kim terefinden yaradilmishdir?.",
+    answers: {
+      A: "A) Pascal",
+      B: "B) Sergey Brin və Lari Peyc",
+      C: "C) Lutfi Zade",
+      D: "D) Nesireddin Tusi",
+    },
+    correct: function () {
+      return this.answers.B;
+    },
+    subject: "İnformatika",
+    level: "Cetin",
+  },
+  {
+    question: "3. Vektor qrafikasinin elementar obyekti nedir?.",
+    answers: {
+      A: "A) splayn",
+      B: "B) kontur",
+      C: "C) siniq",
+      D: "D) xett",
+    },
+    correct: function () {
+      return this.answers.D;
+    },
+    subject: "İnformatika",
+    level: "Cetin",
+  },
+  {
+    question: "4. Ashagidakilardan hansi komputer qrafikasinin tetbiq sahesi deyil?",
+    answers: {
+      A: "A) tesviri incesenet",
+      B: "B) konstruktor qrafika",
+      C: "C) ishguzar qrafika",
+      D: "D) bedii qrafika",
+    },
+    correct: function () {
+      return this.answers.D;
+    },
+    subject: "İnformatika",
+    level: "Cetin",
+  },
+  {
+    question: "5. Fiberoptik rabite kanalinda oturucu muhit ashagidakilardan hansidir?",
+    answers: {
+      A: "A) maye",
+      B: "B) vakuum",
+      C: "C) shushe",
+      D: "D) mis naqil",
+    },
+    correct: function () {
+      return this.answers.C;
+    },
+    subject: "İnformatika",
+    level: "Cetin",
+  },
+
+  
+  
+];
+
+
+
+let filterSubject, filterLevel;
+document.querySelectorAll(".tabOne").forEach((x) => {
+  x.addEventListener("click", function () {
+    document.querySelector(".tabBoxOne").style.display = "none";
+    document.querySelector(".tabBoxTwo").style.display = "block";
+    filterSubject = questionBank.filter(
+      (s) => s.subject.toLowerCase() === x.innerText.toLowerCase()
+    );
+  });
+});
+document.querySelectorAll(".tabTwo").forEach((x) => {
+  x.addEventListener("click", function () {
+    document.querySelector(".tabBoxTwo").style.display = "none";
+    document.querySelector(".quizBoxOne").style.display = "block";
+    filterLevel = filterSubject.filter(
+      (l) => l.level.toLowerCase() === x.innerText.toLowerCase()
+    );
+    getData(0);
+  });
+});
+let correct_count=0,wrong_count=0;
+function getData(index) {
+  document.querySelector(".pageBox").innerHTML = "";
+  document.querySelector(".questionBoxOne").innerText =
+    filterLevel[index].question;
+    document.querySelector(".optionsBoxOne").innerHTML=`
+                        <div class="optionBoxOne">${filterLevel[index].answers.A}</div>
+                        <div class="optionBoxOne">${filterLevel[index].answers.B}</div>   
+                        <div class="optionBoxOne">${ filterLevel[index].answers.C}</div>
+                        <div class="optionBoxOne">${ filterLevel[index].answers.D}</div>`
+  for (let i = 0; i < filterLevel.length; i++) {
+    document.querySelector(
+      ".pageBox"
+    ).innerHTML += `<div class="pageListBox" data-index=${i} onclick="page(${i})">${
+      i + 1
+    }</div>`;
+  }
+  document
+    .querySelector(".pageBox")
+    .children[index].classList.add("btn-active");
+
+  let cavablar = document.querySelector(".optionsBoxOne").children;
+  Array.from(cavablar).forEach(s=>{
+   s.style.backgroundColor="transparent"
+   s.style.pointerEvents="all"
+  })
+  Array.from(cavablar).forEach((c) => {
+    c.addEventListener("click", function () {
+      if (c.innerText === filterLevel[index].correct()) {
+        c.style.backgroundColor = "green";
+        correct_count++
+        console.log(correct_count,"correct");
+        
+      }else {
+         c.style.backgroundColor = "red";
+         wrong_count++
+         console.log(wrong_count,"wrong");
+         Array.from(cavablar).forEach((x) => {
+            if (x.innerText === filterLevel[index].correct()) {
+               x.style.backgroundColor = "green";
+            }
+         });
+      }
+      Array.from(cavablar).forEach(z=>{
+         z.style.pointerEvents="none"
+      })
+    });
+  });
+}
+function page(index) {
+  getData(index);
+}
+
+document.querySelector(".arrowLeft").addEventListener("click",function(){
+   let activebtn= document.querySelector(".pageBox div.btn-active")
+   let index=activebtn.getAttribute("data-index")
+   if(index<=0){
+      return;
+   }else{
+      getData((index-1))   
+   }
+})
+
+document.querySelector(".arrowRight").addEventListener("click",function(){
+   let activebtn= document.querySelector(".pageBox div.btn-active")
+   let index=activebtn.getAttribute("data-index")
+   if(index>=filterLevel.length-1){
+      document.querySelector(".quizBoxOne").style.display= "none";
+      resultsContainer.style.display="block"
+      correct.innerText=correct_count
+      wrong.innerText=wrong_count
+      blank.innerText= filterLevel.length - (correct_count+wrong_count)
+      result.innerText=(correct_count/filterLevel.length)*100 +"%"
+   }else{
+      getData((+index+1))   
+
+   }
+})
+
+
+
+
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,327 @@
+
+// Dom one 
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const addButton= document.querySelector(".addButton");
+//     const inputBox= document.querySelector(".inputBox");
+//     const todoList= document.querySelector(".todoList");
+
+//     addButton.addEventListener("click", ()=> {
+//         const todoText= inputBox.value;
+
+//         const todoItem= document.createElement("li");
+//         todoItem.className= "todoItem";
+//         todoItem.innerHTML= `
+//             <span class="todoText">${todoText}</span>
+//             <button class="delButton">
+//                 <i class="fa-solid fa-trash"></i>
+//             </button>
+//         `;
+
+//         todoList.appendChild(todoItem);
+
+//         const delButton= todoItem.querySelector(".delButton");
+//         delButton.addEventListener("click", ()=> {
+//             todoItem.remove();
+//         });
+
+//         inputBox.value= "";
+//     });
+
+//     document.querySelectorAll(".delButton").forEach(button=> {
+//         button.addEventListener("click", (event)=> {
+//             event.target.closest("li").remove();
+//         });
+//     });
+// });
+
+
+
+// Dom two 
+
+
+
+// const mainImage= document.getElementById('mainImage');
+// const thumbnails= document.querySelectorAll('.footBox .itemBox img');
+// const prevButton= document.getElementById('prev');
+// const nextButton= document.getElementById('next');
+
+// let currentIndex= 0;
+
+// const images= [
+//     'https://www.kate.net/funstuff/wallpapers/katenet-pinkflowerwallpaper2011_1600.jpg',
+//     'https://t4.ftcdn.net/jpg/05/82/97/05/360_F_582970551_V9zUmJdiPUCyownaKd0OWWI6ZBMoH7WH.jpg',
+//     'https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+//     'https://img.pikbest.com/origin/09/08/82/pIkbEsT4IpIkbEsTz3f.jpg!sw800',
+//     'https://t3.ftcdn.net/jpg/05/73/42/58/360_F_573425894_JmV1xE7VCHHJD2phP0v18ukrk9HZN5m7.jpg',
+//     'https://www.shutterstock.com/image-photo/arrangement-decoration-chinese-new-year-600nw-768084829.jpg'
+// ];
+
+// function showImage(index) {
+//     mainImage.src= images[index];
+// }
+
+// thumbnails.forEach((thumbnail, index)=> {
+//     thumbnail.src= images[index];
+//     thumbnail.addEventListener('click', ()=> {
+//         currentIndex= index;
+//         showImage(currentIndex);
+//     });
+// });
+
+// prevButton.addEventListener('click', ()=> {
+//     currentIndex= (currentIndex > 0) ? currentIndex- 1 : images.length- 1;
+//     showImage(currentIndex);
+// });
+
+// nextButton.addEventListener('click', ()=> {
+//     currentIndex= (currentIndex < images.length- 1) ? currentIndex+ 1 : 0;
+//     showImage(currentIndex);
+// });
+
+// function startSlideshow() {
+//     setInterval(()=> {
+//         currentIndex= (currentIndex < images.length- 1) ? currentIndex+ 1 : 0;
+//         showImage(currentIndex);
+//     }, 4000);
+// }
+
+// showImage(currentIndex);
+// startSlideshow();
+
+
+
+// Dom three 
+
+
+
+// let images= [
+//     {category:"nature", name:"forest", source:"./assets/image/forest.jpg"},
+//     {category:"nature", name:"mountain", source:"./assets/image/mountains.jpg"},
+//     {category:"nature", name:"light", source:"./assets/image/lights.jpg"},
+//     {category:"car", name:"retro", source:"./assets/image/retro.jpg"},
+//     {category:"car", name:"fast", source:"./assets/image/fast.jpg"},
+//     {category:"car", name:"classic", source:"./assets/image/classic.jpg"},
+//     {category:"people", name:"girl", source:"./assets/image/girl.jpg"},
+//     {category:"people", name:"man", source:"./assets/image/man.jpg"},
+//     {category:"people", name:"woman", source:"./assets/image/woman.jpg"},
+// ]
+
+// function filterImages(category) {
+//     let gallery= document.getElementById('gallery');
+//     gallery.innerHTML= '';
+
+//     let filteredImages= images.filter(image=> category=== 'all' || image.category=== category);
+
+//     filteredImages.forEach(image=> {
+//         let card= document.createElement('div');
+//         card.className= 'card';
+//         card.innerHTML= `
+//             <img src="${image.source}" alt="${image.name}">
+//             <div class="card-body">
+//                 <p>${image.name.charAt(0).toUpperCase()+ image.name.slice(1)}</p>
+//                 <p>Lorem ipsum dolor...</p>
+//             </div>`;
+//         gallery.appendChild(card);
+//     });
+
+//     let buttons= document.querySelectorAll('.textBox p');
+//     buttons.forEach(button=> button.classList.remove('selected'));
+//     document.querySelector(`.textBox p[onclick="filterImages('${category}')"]`).classList.add('selected');
+// }
+
+// filterImages('all');
+
+
+
+// Don four 
+
+
+
+// let volume= document.querySelector(".parentBox");
+//     let volume_bar= document.querySelector(".childBox");
+//     let volume_width= volume.clientWidth;
+//     let faiz= document.getElementById("faizBox");
+        
+// function updateVolume(percent) {
+//     volume_bar.style.width= percent+ "%";
+//     faiz.innerText= percent+ " %";
+// }
+//     volume.addEventListener("click", function(e) {
+//         let percent= Math.round((e.offsetX/ volume_width)* 100);
+//         updateVolume(percent);
+//     });
+
+//     document.getElementById("plus").addEventListener("click", function() {
+//         let percent= Math.min(100, Math.round((volume_bar.clientWidth/ volume_width)* 100)+ 1);
+//         updateVolume(percent);
+//     });
+
+//     document.getElementById("minus").addEventListener("click", function() {
+//         let percent= Math.max(0, Math.round((volume_bar.clientWidth/ volume_width)* 100)- 1);
+//         updateVolume(percent);
+//     });
+
+//     updateVolume(55);
+
+
+
+// Dom five 
+
+
+
+// const content= document.getElementById('contentBoxThree');
+// const pages= document.querySelectorAll('.pagination .page');
+// const prev= document.querySelector('.pagination .prev');
+// const next= document.querySelector('.pagination .next');
+// let currentPage= 1;
+// const itemsPerPage= 10;
+
+// const data= [];
+
+// function addData(item) {
+//     data.push(item);
+// }
+
+// addData("Adam 1");
+// addData("Badam 2");
+// addData("Meymun 3");
+// addData("Alimaz 4");
+// addData("Item 5");
+
+// for (let i= data.length; i< 50; i++) {
+//     data.push(`Item ${i + 1}`);
+// }
+
+// function displayItems(page) {
+//     content.innerHTML= '';
+//     const start= (page - 1) * itemsPerPage;
+//     const end= page * itemsPerPage;
+//     const itemsToDisplay= data.slice(start, end);
+
+//     itemsToDisplay.forEach(itemText=> {
+//         const item= document.createElement('li');
+//         item.textContent= itemText;
+//         content.appendChild(item);
+//     });
+// }
+
+// function updatePagination(page) {
+//     pages.forEach(p=> p.classList.remove('active'));
+//     pages[page- 1].classList.add('active');
+// }
+
+// pages.forEach((page, index)=> {
+//     page.addEventListener('click', ()=> {
+//         currentPage= index+ 1;
+//         displayItems(currentPage);
+//         updatePagination(currentPage);
+//     });
+// });
+
+// prev.addEventListener('click', ()=> {
+//     if (currentPage> 1) {
+//         currentPage--;
+//         displayItems(currentPage);
+//         updatePagination(currentPage);
+//     }
+// });
+
+// next.addEventListener('click', ()=> {
+//     if (currentPage< pages.length) {
+//         currentPage++;
+//         displayItems(currentPage);
+//         updatePagination(currentPage);
+//     }
+// });
+
+// displayItems(currentPage);
+// updatePagination(currentPage);
+
+
+
+
+// const content= document.getElementById('contentBoxThree');
+// const pages= document.querySelectorAll('.pagination .page');
+// const prev= document.querySelector('.pagination .prev');
+// const next= document.querySelector('.pagination .next');
+// let currentPage= 1;
+// const itemsPerPage= 10;
+
+// const data= [];
+
+// function addData(item) {
+//     data.push(item);
+// }
+
+// addData("Adam 1");
+// addData("Badam 2");
+// addData("Meymun 3");
+// addData("Almaz 4");
+// addData("Item 5");
+
+// for (let i= data.length; i< 50; i++) {
+//     data.push(`Item ${i + 1}`);
+// }
+
+// function displayItems(page) {
+//     content.innerHTML= '';
+//     const start= (page - 1)* itemsPerPage;
+//     const end= page* itemsPerPage;
+//     const itemsToDisplay= data.slice(start, end);
+
+//     itemsToDisplay.forEach(itemText=> {
+//         const item= document.createElement('li');
+//         item.textContent= itemText;
+//         content.appendChild(item);
+//     });
+// }
+
+// function updatePagination(page) {
+//     pages.forEach(p => p.classList.remove('active'));
+//     pages[page - 1].classList.add('active');
+
+//     if (page=== 1) {
+//         prev.classList.add('disabled');
+//     }else {
+//         prev.classList.remove('disabled');
+//     }
+
+//     if (page=== pages.length) {
+//         next.classList.add('disabled');
+//     }else {
+//         next.classList.remove('disabled');
+//     }
+// }
+
+// pages.forEach((page, index)=> {
+//     page.addEventListener('click', ()=> {
+//         currentPage= index+ 1;
+//         displayItems(currentPage);
+//         updatePagination(currentPage);
+//     });
+// });
+
+// prev.addEventListener('click', ()=> {
+//     if (currentPage> 1) {
+//         currentPage--;
+//         displayItems(currentPage);
+//         updatePagination(currentPage);
+//     }
+// });
+
+// next.addEventListener('click', ()=> {
+//     if (currentPage< pages.length) {
+//         currentPage++;
+//         displayItems(currentPage);
+//         updatePagination(currentPage);
+//     }
+// });
+
+// displayItems(currentPage);
+// updatePagination(currentPage);
+
+
+
